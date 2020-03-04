5a9fbba9d5b46fc8f24d8483676236984fffe576
xiaomanDou <61768387+xiaomanDou@users.noreply.github.com>
2020-03-04 22:21:15 +0800

    提交代码

--- fibonacci.c
+++ fibonacci.c
@@ -1,1 +1,16 @@
-
+# include <stdio.h>
+int Fibonacci(int n) 
+{
+	if (n==1 || n==2)
+	return 1;
+	else return Fibonacci(n-1)+Fibonacci(n-2);
+	
+}
+int main()
+{
+	int i;
+	for (i=1;i<=200;i++)
+	printf("%d\n",Fibonacci(i));
+	return 0;
+	
+ } 
